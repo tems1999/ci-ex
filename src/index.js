6bc82511323703e12290476f55ab8cb602480c62
Noppawin Tems <noppawin@test.com>
2025-09-02 04:03:48 +0000

    Implement CodeQL

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const add = (a, b) => {
   //const result = a+b;
   //return result;
   //CodeQL
-  return eval(`${a} + ${b}`);
+  return eval(`${a} + ${b}`); //ช่องโหว่คือ Code Injection / Remote Code Execution (RCE) เพราะ eval เปิดโอกาสให้ attacker ใส่โค้ดอันตรายเข้ามารันได้
 };
 export default add;
